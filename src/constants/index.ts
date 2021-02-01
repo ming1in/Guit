64ea5d8f382148800ca577d8d65f387461349b9b
ming1in <minglianglin50@gmail.com>
2021-02-01 18:53:44 -0500

    stuff

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export enum Zillow {
+  zwsid = 'X1-ZWz1i5wgouoyyz_634pb',
+  getSearchResultApi = 'http://www.zillow.com/webservice/GetSearchResults.htm'
+}
--- src/parsers/index.tsx
+++ src/parsers/index.tsx
@@ -1,56 +1,64 @@
-import rp from 'request-promise'
-import cheerio from 'cheerio'
-// import { RequestError } from 'request-promise/errors';
+import axios from 'axios'
+import { Zillow } from '../constants/index'
 
-// const getAddress = (url: string) => {
-//   const splitUrl = url.split('/')[4]
-//   const splitAddress = splitUrl.split('-')
-//   const splitAddressLength = splitAddress.length
 
-//   return {
-//     state: splitAddress[splitAddressLength - 2],
-//     zip: splitAddress[splitAddressLength - 1],
-//     city: splitAddress[splitAddressLength - 3],
-//     address: splitAddress.slice(0, splitAddressLength - 3).join('-')
-//   }
-// }
-
-// interface Listing{
-//   url: string,
-//   price: number,
-//   bedroom: number,
-//   sqft: number,
-//   state: string,
-//   zip: number,
-//   city: string,
-//   address: string
-// }
-
-export const LinkParser = (url: string) => {
-
-  const params = {
-    uri: url,
-    transform: function (body: any) {
-      return cheerio.load(body);
+export function LinkParser(link: string) {
+  var homeDetails
+  var zpid
+  const split = link.split('/')
+    
+  split.forEach((el: string, idx) => {
+    if (el === 'homedetails') {
+      homeDetails = split[idx + 1]
     }
-  }
 
-  rp(params).then((html) => {
-    console.log(html)
-  }).catch((error: any) =>{
-    console.log(error)
+    if (el.split('_')[1] === 'zpid') {
+      zpid = el.split('_')[0]
+    }
   })
+  
+  // fetch(`http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=${Zillow.zwsid}&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA`, {
+  //   mode: 'no-cors'
+  // })
+  //   .then((response) => console.log(response))
+  //   .catch((err) => console.log(err))
+  
+
+  // axios.get('http://www.zillow.com/webservice/GetSearchResults.htm', {
+  //   params: {
+  //     'zws-id': Zillow.zwsid,
+  //     'address': '2114 Bigelow Ave',
+  //     'citystatezip': 'Seattle, WA'
+  //   },
+  //   headers: {
+  //     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+  //     'accept-encoding': 'gzip, deflate, br',
+  //     'accept-language': 'en-US,en;q=0.9',
+  //     'sec-fetch-mode': 'navigate',
+  //     'sec-fetch-site': 'none',
+  //     'upgrade-insecure-requests': '1'
+  //   }
+  // })
+  //   .then((response) => console.log(response))
+  //   .catch((err) => console.log(err))
 
 
+  const request = new XMLHttpRequest()
 
-      // var home = {
-      //   url: url,
-      //   price: $('h4.Text-c11n-8-18-0__aiai24-0.StyledHeading-c11n-8-18-0__ktujwe-0.gcaUyc.sc-qZtCU.kijwKF', html)[0].children[0].children[0].children[0].data,
-      //   bedroom: $('h3.ds-bed-bath-living-area-container', html)[0].children[0].children[0].children[0].data,
-      //   bathroom: $('h3.ds-bed-bath-living-area-container', html)[0].children[2].children[0].children[0].children[0].data,
-      //   sqft: $('h3.ds-bed-bath-living-area-container', html)[0].children[4].children[0].children[0].data,
-      //   ...getAddress(url)
-      // }
+  request.open('GET', `http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=${Zillow.zwsid}&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA`, true)
 
 
+  request.setRequestHeader('Access-Control-Allow-Origin', '*')
+
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+      
+      console.log(request.responseText)
+    }
+  };
+
+  request.send()
+  
 }
+
